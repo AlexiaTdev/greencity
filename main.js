6e11bfbe1c8951853771a2746de600b22fc96cb9
sasha-loubet <73692446+sasha-loubet@users.noreply.github.com>
2020-11-05 14:11:36 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,77 @@
+let playing=false;
+let score=0;
+
+
+document.getElementById('start').onclick=function(){
+    //location.reload();
+    generatetxt('carte.xml');
+    generatetxt()
+}
+/**
+document.getElementById('bouton').onclick=function(){
+    re  affiche les stat en cours
+}*/
+
+function generatetxt(nomFichier) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            chargement(this);
+        }
+    };
+    xhttp.open("GET", nomFichier, true);
+    xhttp.send();
+}
+
+//fonction pour les cercles des stats
+
+function Circlle(el){
+              
+    $(el).circleProgress({fill:{color:'#ff5c5c'}})
+    .on('circle-animation-progress', function(event, progress, stepValue){
+
+      $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%');
+     });
+ };
+
+ Circlle('.round');
+
+
+function chargement(xml) {
+    //recupere les cartes
+    var xmlDoc = xml.responseXML;
+
+    //on choisis la carte
+    cardNumber=0;
+
+    //on récupère la carte
+    cartes = xmlDoc.getElementsByTagName('carte');
+    question= cartes[cardNumber].childNodes[1].textContent;
+    prop1=cartes[cardNumber].getElementsByTagName('proposition')[0].childNodes[0].nodeValue;
+    prop2=cartes[cardNumber].getElementsByTagName('proposition')[1].childNodes[0].nodeValue;
+
+    //On affiche les elements dans le html
+    document.getElementById('note').textContent = question;
+    for (i=0; i<4; i++) {
+        document.getElementById('option'+ (i+1)).textContent = cartes[cardNumber].getElementsByTagName('proposition')[i].childNodes[0].nodeValue;
+    }
+
+    //recupere les stats
+    var xmlDoc2 = xml.responseXML;
+
+    //on affiche les stat
+    stataffichee = xmlDoc2.getElementsByTagName('stat');
+    //trafficvoitureelectrique = voitureelectrique
+    console.log(stataffichee);
+
+
+}
+
+
+
+/**
+function gameOver()
+{
+    document.getElementById('gameover').style.display="block";
+    document.getElementById('scoreno').innerHTML=score;
+}*/
